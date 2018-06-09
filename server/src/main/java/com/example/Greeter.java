f4359e35d1bcf7692f0c809e14bfb5348e7615c0
Shivakumar <charanya-k.shivakumar@hpe.com>
2018-06-10 00:34:34 +0530

    fix chkstyle warns

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
 * @param someone the name
 * @return greeting stirng
 */
-public final String greet( final String someone) {
+public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
